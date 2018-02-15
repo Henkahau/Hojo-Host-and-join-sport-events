6dfff4a3d91feac3cdfcd708fde8c03eade191bc
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-02-15 11:26:16 +0200

    Eka commit

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
