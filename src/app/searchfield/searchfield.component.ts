b20993545231e124bbaa10257a911b4b2f2a4fab
Unknown <t5imva00@students.oamk.fi>
2018-03-16 11:14:19 +0200

    Deleted unnecessary
    Deletet hardcoded markers

--- src/app/searchfield/searchfield.component.ts
+++ src/app/searchfield/searchfield.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Activity } from './activity';
 
 @Component({
   selector: 'app-searchfield',
@@ -6,13 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchfield.component.css']
 })
 export class SearchfieldComponent implements OnInit {
-  laji:string
+  
 
- sports: string[] = ['Icehockey', 'Basketball', 'Tennis', 'Running'];
+
  locations: string [] = [ 'Oulu', 'Rajakylä', 'Raahe', 'Tampere'];
  skillLevels: string[] = ['Professional', 'Semi-pro','amateur'];
  amountOfPeople:  number []= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
-  ngOnInit() {
+ activity:Activity[]; 
+ activitySelected: number;
+ message: number;
+ @Output () messageEvent = new EventEmitter<number>();
+ 
+ 
+ ngOnInit() {
+  this.activity = [
+    { id:1, sport: "Tennis"},
+    { id:2, sport: "Icehockey"},
+    { id:3, sport: "Running"}
+  ];
+  this.activitySelected = 3;
+
+  }
+  onActivitySelected(val:any)
+  {
+
+    this.sendMessage(val);
+  }
+
+  sendMessage(val:any)
+  {
+  
+    this.message =   val;
+    this.messageEvent.emit(this.message);
   }
 
 }
