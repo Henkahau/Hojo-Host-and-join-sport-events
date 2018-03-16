817d2fbb49c65e24b7c3df5cd9cabfe5b9e8e605
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-03-16 10:03:06 +0200

    Create event

--- src/app/event/create-event/create-event.component.ts
+++ src/app/event/create-event/create-event.component.ts
@@ -7,22 +7,22 @@ import { EventService, AlertService } from '../../_services/index';
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.css'],
   moduleId: module.id
 })
+
 export class CreateEventComponent implements OnInit {
   model: any = {};
   loading = false;
-  sportTypes: SportType;
-
+  sportValues = Object.values(SportType);
+  
   constructor(
       private router: Router,
       private eventService: EventService,
-      private alertService: AlertService) { }
+      private alertService: AlertService) {}
 
   ngOnInit() {
   }
-
+ 
   createEvent(){
     this.loading = true;
     this.eventService.createEvent(this.model)
@@ -31,13 +31,13 @@ export class CreateEventComponent implements OnInit {
             //set succes message and pass true parameter to persist teh message after redirectin to the main page
             this.alertService.success('Event created succesfull')
             //navigate to main page..
-            this.router.navigate(['/home']);
+            this.router.navigate(['/']);
           },
           error => {
             this.alertService.error(error);
             this.loading = false;
           }
         );
-  }
+  } 
 
 }
