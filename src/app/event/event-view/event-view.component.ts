66ed11c4a8d00f01d84030d3285abeb8a3b7e02c
Atte Karppinen <t6kaat01@students.oamk.fi>
2018-03-19 09:25:53 +0200

    Expecting inputs
    Plus JoinEvent function

--- src/app/event/event-view/event-view.component.ts
+++ src/app/event/event-view/event-view.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { EventService } from '../../_services/index';
+import { Router } from '@angular/router';
+import { MapComponent } from '../../map/index';
 
 @Component({
   selector: 'app-event-view',
@@ -9,19 +11,35 @@ import { EventService } from '../../_services/index';
 })
 export class EventViewComponent implements OnInit 
 {
-  ifSignedIn = true;  
+  signedIn = true;
 
-  private eventService: EventService;
+  // These ids will be received when user clicks marker/event
+  @Input() eventID: number;
+  @Input() accountID: number;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private eventService: EventService,
+    ) { }
 
   ngOnInit() {
   }
 
-  delete()
+  deleteEvent()
   {
-    // Replace number 1 with actual event id
-    this.eventService.deleteEvent(1);
+    this.eventService.deleteEvent(this.eventID);
   }
 
+  joinEvent()
+  {
+    // This will be also used to leave event
+    this.eventService.joinEvent(this.eventID, this.accountID);
+    this.signedIn = !this.signedIn;
+  }
+
+  close()
+  {
+    // Not necessary when using modal window
+    this.router.navigate(['/']);
+  }
 }
