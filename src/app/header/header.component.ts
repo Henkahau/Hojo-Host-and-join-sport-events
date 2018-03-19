99e858f301965d2a92a95b6826d5a98d5e97c85d
Atte Karppinen <t6kaat01@students.oamk.fi>
2018-03-19 15:10:45 +0200

    Switch case to navigate with navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent{
 
-    constructor() {}
+    constructor(private router: Router) {}
+
+    onSelect(feature: string)
+    {
+        switch (feature)
+        {
+            case 'create': {
+                this.router.navigate(['/createEvent']);
+                break;
+            }
+            case 'login': {
+                this.router.navigate(['/login']);
+                break;
+            }
+            case 'register': {
+                this.router.navigate(['/register']);
+                break;
+            }
+        }
+    }
 }
