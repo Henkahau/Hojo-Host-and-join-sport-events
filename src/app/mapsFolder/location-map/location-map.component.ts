d3ade9e84bc4ce9e12cdca55d7865388a503048b
Atte Karppinen <t6kaat01@students.oamk.fi>
2018-03-22 16:58:01 +0200

    Draggable marker on createEvent

--- src/app/mapsFolder/location-map/location-map.component.ts
+++ src/app/mapsFolder/location-map/location-map.component.ts
@@ -9,37 +9,17 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./location-map.component.css']
 })
 export class LocationMapComponent implements OnInit {
-  markers = [{
-    longitude: 25.6,
-    latitude: 65.5,
-    tag: 1,
-  },
-  {
-
-    longitude: 24.9384,
-    latitude: 60.1699,
-    tag: 'Golf'
-  },
-  {
-    longitude: 23.45,
-    latitude: 61.29,
-    tag: 'Golf'
-  }];
 
   public latitude: number;
   public longitude: number;
+  public latitudeMarker: number;
+  public longitudeMarker: number;
 
   public searchControl: FormControl;
   public zoom: number;
-  message;
-  message2;
-  displayMarkker = true;
-  displayMarkker2 = true;
-  windowinfo = false;
 
   @ViewChild("searchLocation")
   public searchElementRef: ElementRef;
-
 
   constructor(
     private mapsAPILoader: MapsAPILoader,
@@ -50,11 +30,6 @@ export class LocationMapComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
-    this.zoom = 4;
-    this.latitude = 65.0121;
-    this.longitude = 25.4651;
-
 
     //create search FormControl
     this.searchControl = new FormControl();
@@ -78,6 +53,8 @@ export class LocationMapComponent implements OnInit {
           //set latitude, longitude and zoom
           this.latitude = place.geometry.location.lat();
           this.longitude = place.geometry.location.lng();
+          this.latitudeMarker = this.latitude;
+          this.longitudeMarker = this.longitude;
           this.zoom = 12;
         });
       });
@@ -89,13 +66,26 @@ export class LocationMapComponent implements OnInit {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
+        this.latitudeMarker = this.latitude;
+        this.longitudeMarker = this.longitude;
         this.zoom = 12;
       });
     }
   }
 
-  onChoseLocation(event) {
-    this.latitude = event.coords.lat;
-    this.longitude = event.coords.lng;
+  moveMarker(event: any)
+  {
+    this.latitudeMarker  = event.coords.lat;
+    this.longitudeMarker = event.coords.lng;
+    console.log('Click: ' + event.coords.lat);
+    console.log('Click: ' + event.coords.lng);
+  }
+
+  dragEnd(event: any)
+  {
+    this.latitudeMarker  = event.coords.lat;
+    this.longitudeMarker = event.coords.lng;
+    console.log('Drag: ' + event.coords.lat);
+    console.log('Drag: ' + event.coords.lng);
   }
 }
