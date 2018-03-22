1a083bc5c786e71ceaf527e0e5ac1ad12523ec47
Unknown <t5imva00@students.oamk.fi>
2018-03-22 09:00:49 +0200

    ngIf in navbar
    if you havent logged in all options dont show in navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent{
+    loggaIn = true;
 
     constructor(private router: Router) {}
 
