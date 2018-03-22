3c8bb4a24ddca232d577d9544b16ccbc691d9219
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-03-22 12:34:20 +0200

    loads specific event for editing

--- src/app/event/edit-event/edit-event.component.ts
+++ src/app/event/edit-event/edit-event.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../_services/index';
+import { Event } from '../../_models/index';
 
 @Component({
   selector: 'app-edit-event',
   templateUrl: './edit-event.component.html',
-  styleUrls: ['./edit-event.component.css']
+  styleUrls: ['./edit-event.component.css'],
+  moduleId: module.id
 })
 export class EditEventComponent implements OnInit {
 
-  constructor() { }
+  event: Event;
+  static editEventId: string;
+
+  constructor(private eventService: EventService) { }
 
   ngOnInit() {
+    this.loadEvent();
+  }
+
+  private loadEvent(){
+    this.eventService.getEventById(EditEventComponent.editEventId).subscribe(event => { this.event = event });
   }
 
 }
