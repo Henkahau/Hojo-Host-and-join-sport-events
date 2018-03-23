66e0bdacb9319880b6b22f8ca70f230e08311686
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-03-23 13:24:56 +0200

    Added module id

--- src/app/user-profile/edit-profile/edit-profile.component.ts
+++ src/app/user-profile/edit-profile/edit-profile.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css']
+  styleUrls: ['./edit-profile.component.css'],
+  moduleId: module.id
 })
 export class EditProfileComponent implements OnInit {
 
