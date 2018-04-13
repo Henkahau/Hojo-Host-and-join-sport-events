a40d3df2926edc3c2480c472da22b2839815926e
Unknown <t5imva00@students.oamk.fi>
2018-04-13 14:12:41 +0300

    Housekeeping

--- src/app/searchfield/searchfield.component.ts
+++ src/app/searchfield/searchfield.component.ts
@@ -10,11 +10,6 @@ import { EventService } from '../_services';
 })
 export class SearchfieldComponent implements OnInit {
   
-
-
-// locations: string [] = [ 'Oulu', 'Rajakylä', 'Raahe', 'Tampere'];
- skillLevels: string[] = ['Professional', 'Semi-pro','amateur'];
- amountOfPeople:  number []= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
  activity:Activity[]; 
  activitySelected: number;
  message: number;
@@ -23,41 +18,14 @@ export class SearchfieldComponent implements OnInit {
  sportValues = Object.values(SportType);
  skillValues = Object.values(SkillLevel);
  playTypeValues = Object.values(PlayType);
-
  eventInfo: any = {};
  
- constructor(private eventService: EventService)
-{
+ constructor(private eventService: EventService){} 
 
-} 
- ngOnInit() {
-  // this.activity = [
-  //   { id:1, sport: "Tennis"},
-  //   { id:2, sport: "Icehockey"},
-  //   { id:3, sport: "Running"}
-  // ];
- // this.activitySelected = 3;
+ ngOnInit() {}
 
-  }
-  onActivitySelected(val:any) {
-  //  this.sendMessage(val);
-   
+  onActivitySelected(val:any)
+  {
     this.messageEvent.emit(this.eventInfo);
-  }
-  onSkillLevelSelected(val2:any)
-  {
-    this.sendMessage(val2)
-  }
-
-  sendMessage(val:any)
-  {
-  
-    this.message =   val;
-  
-    
-  }
-  sendLevel(val2: any){
-    this.message2 = val2;
-    this.messageEvent.emit(this.message2)
   }
 }
