ec17404d2a7298b286503998669ad83975d5b94d
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-04-22 20:52:35 +0300

    UserProfile eventView

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -3,6 +3,10 @@ import { User, Event } from '../_models/index'
 import { UserService, AlertService, AuthenticationService, EventService } from '../_services';
 import { Router } from '@angular/router';
 
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { EventViewComponent } from '../event/event-view';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -14,17 +18,21 @@ export class UserProfileComponent implements OnInit {
   
   model: any = {};
   currentUser: User;
+  id: string;
   hostedEvents: Event[];
   joinedEvents: Event[];
   userProfileId: string;
-  imagePath: string = '../../assets/Images/default_profile_image.png'; 
+  imagePath: string = '../../assets/Images/default_profile_image.png';
+
+  modalRef: BsModalRef;
 
   constructor(private userService: UserService,
               private authenticationService: AuthenticationService,
               protected eventService: EventService,
+              private modalService: BsModalService,
               private router: Router) {
                  var currUser = JSON.parse(localStorage.getItem('currentUser'));
-                 this.currentUser = currUser.Account;
+                 this.id = currUser.Account.accountId;
                 
                  EventService.refreshEventList.subscribe(res =>{
                    this.loadProfile();
@@ -33,15 +41,22 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit() {
     this.loadProfile();
-    console.log(this.currentUser.profilePicture);
   }
 
   private loadProfile(){
-    this.userService.getById(this.currentUser.accountId).subscribe(user => {
+    this.userService.getById(this.id).subscribe(user => {
       this.currentUser = user[0];
-      this.joinedEvents = this.currentUser.events;
-      this.hostedEvents = this.currentUser.hostedEvents;     
+
+      this.joinedEvents = this.currentUser.events
+      this.hostedEvents = this.currentUser.hostedEvents;
+
     });
+  }
+
+  private openEventView(id: string) {
+    //OPENING EVENTVIEW HAS SOME ERROR
+    /* sessionStorage.setItem("eventId", id);
+    this.modalRef = this.modalService.show(EventViewComponent, { class: 'modal-lg' }); */
   }
 
   navigate(destination: string) {
