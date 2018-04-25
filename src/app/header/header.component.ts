fcf35e316828af5fa0ca02158475943cccf21ffb
Henri Haukipuro <henri.haukipuro@kotinet.com>
2018-04-25 10:40:19 +0300

    header profile picture

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,13 @@ import { Component, Output, HostListener, HostBinding } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { Event } from '../_models/index';
+import { Event, User } from '../_models/index';
 import { UserService, EventService, AuthenticationService } from '../_services/index';
 import { CreateEventComponent } from '../event/index';
 import { LoginComponent } from '../login';
 import { RegisterComponent } from '../register';
 import { UserProfileComponent } from '../user-profile';
+
 
 @Component({
     selector: 'app-header',
@@ -21,13 +22,20 @@ export class HeaderComponent{
     @Output ()public imagePath: string = '../../assets/Images/default_profile_image.png';
     @Output ()public imagePath2: string = '../../assets/Images/logo.png'; 
     modalRef: BsModalRef;
-    options = ['Options','Create Event', 'My Profile', 'Logout']
+    options = ['Options','Create Event', 'My Profile', 'Logout'];
+    currentUser: User;
   
     constructor(
         private router: Router,
         private modalService: BsModalService,
         private eventService: EventService,
-        private authenticationService: AuthenticationService) {}
+        private authenticationService: AuthenticationService) {
+            var currUser = JSON.parse(localStorage.getItem('currentUser'));
+            this.currentUser = currUser.Account;
+            if(this.currentUser.profilePicture != null){
+                this.imagePath = this.currentUser.profilePicture;
+            }
+        }
 
     getUserLoginStatus(): boolean {
         return this.authenticationService.getLoginStatus();
